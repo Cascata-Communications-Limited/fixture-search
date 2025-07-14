cc94fd2b439735285a91b2444972c3d500620625
enoble <enoble@cascatacom.co.uk>
2025-07-14 12:11:18 +0100

    Initial scaffold: Vite setup, documentation, bundling config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+ build: {
+    lib: {
+      entry: './src/components/FixtureSearch.jsx',
+      name: 'FixtureSearch',
+      fileName: 'index',
+      formats: ['es', 'cjs'] // ESModule + CommonJS
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
+});
